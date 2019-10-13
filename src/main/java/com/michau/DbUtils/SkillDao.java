d732f403d162dc9b3334247296e3f3f59254d2cf
apokalipsa44 <m_lachowicz@wp.pl>
2019-10-13 13:42:08 +0200

    dao's created and implemented

--- src/main/java/com/michau/DbUtils/SkillDao.java
+++ src/main/java/com/michau/DbUtils/SkillDao.java
@@ -1,4 +1,26 @@
 package com.michau.DbUtils;
 
-public class SkillDao {
+import com.michau.Model.Skills;
+import org.hibernate.SessionFactory;
+
+public class SkillDao extends BaseDao {
+    public SkillDao(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+
+    public Skills get(Long id) {
+        return super.produceInTransaction(session -> session.get(Skills.class, id));
+    }
+
+    public void save(Skills skill) {
+        super.executeInTransaction(session -> session.save(skill));
+    }
+
+    public void update(Skills skill) {
+        super.executeInTransaction(session -> session.update(skill));
+    }
+
+    public void delete(Skills skill) {
+        super.executeInTransaction(session -> session.delete(skill));
+    }
 }
--- src/main/java/com/michau/DbUtils/SourceDao.java
+++ src/main/java/com/michau/DbUtils/SourceDao.java
@@ -1,5 +1,28 @@
 package com.michau.DbUtils;
 
-public class SourceDao {
+import com.michau.Model.Skills;
+import com.michau.Model.Sources;
+import org.hibernate.SessionFactory;
 
+public class SourceDao extends BaseDao {
+
+    protected SourceDao(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+
+    public Sources get(Long id) {
+        return super.produceInTransaction(session -> session.get(Sources.class, id));
+    }
+
+    public void save(Sources source) {
+        super.executeInTransaction(session -> session.save(source));
+    }
+
+    public void update(Sources source) {
+        super.executeInTransaction(session -> session.update(source));
+    }
+
+    public void delete(Sources source) {
+        super.executeInTransaction(session -> session.delete(source));
+    }
 }
